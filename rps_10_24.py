3a1602f6e146da4af4042c979d24d14304834f3a
Christina L <65386414+christina-ml@users.noreply.github.com>
2020-10-24 23:11:46 -0400

    Add files via upload
    This is a version of Rock, Paper, Scissors that will play a random Computer Player against another random Computer Player.

---
+++ rps_10_24.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+"""This program plays a game of Rock, Paper, Scissors between two Players,
+and reports both Player's scores each round."""
+import random  # Adding for a random move by the RandomPlayer object
+import time  # For time delay, so it doesn't all print at once.
+
+moves = ['rock', 'paper', 'scissors']
+
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(1)
+
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+
+class Player:  # Parent class for all players
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        pass
+
+
+class RandomPlayer(Player):  # Subclass of Player
+    def move(self):
+        return random.choice(moves)  # Picking a move at random
+
+
+def beats(one, two):
+    return ((one == 'rock' and two == 'scissors') or
+            (one == 'scissors' and two == 'paper') or
+            (one == 'paper' and two == 'rock'))
+
+def play_again_question():
+    response = input("Play again? (y/n)")
+    if response == "y":
+        if __name__ == '__main__':
+            game = Game(RandomPlayer(), RandomPlayer())  # Who is playing the game
+            game.play_game()
+    if response == "n":
+        quit()
+    else:
+        play_again_question()
+
+
+class Game:
+    p1_score = 0
+    p2_score = 0
+
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def play_round(self):
+        move1 = self.p1.move()
+        move2 = self.p2.move()
+        print_pause(f"Player 1: {move1}  Player 2: {move2}")
+        self.p1.learn(move1, move2)
+        self.p2.learn(move2, move1)
+        if beats(move1, move2):  # Determining who wins using beats fn
+            self.p1_score += 1
+            print_pause("* * * Player 1 wins this round * * *\n")
+        elif beats(move1, move2):
+            self.p2_score += 1
+            print_pause("* * * Player 2 wins this round * * *\n")
+        else:
+            print_pause("* * * It's a tie this round! * * *\n")
+        print("The current score is:")  # This lets you know the current score
+        print("Player 1:")
+        print_pause(self.p1_score)
+        print("Player 2:")
+        print_pause(self.p2_score)
+        print("\n")
+
+
+        results1 = self.p1_score
+        results2 = self.p2_score
+        if results1 == 3:
+            print("Hooray! Player 1 has won the most rounds!")
+            print("Player 2 has been defeated.\n")
+            play_again_question()
+        if results2 == 3:
+            print("Incredible! Player 2 has won the most rounds!")
+            print("Player 1 has been defeated.\n")
+            play_again_question()
+
+
+    def play_game(self):
+        print_pause("Welcome to Rock, Paper, Scissors!")
+        print_pause("How to play: Choose either rock, paper, or scissors.")
+        print_pause("Rock beats scissors.")
+        print_pause("Paper covers rock.")
+        print_pause("Scissors cuts paper.")
+        print_pause("First player to reach 3 points wins.")
+        print_pause("Game start!")
+        for round in range(1, 20):
+            print(f"• • • Round {round} --> GO!")
+            self.play_round()
+            # print("Game over!")
+
+    # def quit():
+    #     print("Game over! done!")
+
+
+if __name__ == '__main__':
+    game = Game(RandomPlayer(), RandomPlayer())  # Who is playing the game
+    game.play_game()
